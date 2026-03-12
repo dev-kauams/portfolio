1add76d1b7890845dc8ad2150f3c41d88fcbcc4d
dev-kauams <kauams.profissional@gmail.com>
2026-03-12 00:02:21 -0300

    feat: new changes on header, main and footer; new support for smartphone.

---
+++ script.js
@@ -0,0 +1,53 @@
+const navSections = document.querySelector('.nav-sections')
+// NAV OPACITY
+navSections.addEventListener('mouseenter', () => {
+    navSections.style.opacity = "1"
+})
+
+navSections.addEventListener('mouseleave', () => {
+    navSections.style.opacity = "0.1"
+})
+
+
+const sections = document.querySelectorAll("main section")
+const links = document.querySelectorAll(".nav-sections a")
+
+
+const bar = document.querySelector(".selection-bar")
+
+function moveBar(link) {
+
+    bar.style.width = link.offsetWidth + "px"
+    bar.style.transform = `translateX(${link.offsetLeft}px)`
+    links.forEach(l => l.classList.remove("active"))
+    link.classList.add("active")
+
+}
+
+
+// detecta seção visível
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            const id = entry.target.id
+
+            const activeLink = document.querySelector(`.nav-sections a[href="#${id}"]`)
+
+            if (activeLink) {
+                moveBar(activeLink)
+            }
+
+        }
+
+    })
+
+}, {
+    threshold: 0.6
+})
+
+
+// observar todas as sections
+sections.forEach(section => observer.observe(section))
